771257b6bfb54e96ee9334c08e1a37de1461b0c2
zack.zhang <zhangwei>
2019-08-08 10:47:56 +0800

    添加eureka注册服务provider项目2

---
+++ learn_eureka_provider2/src/main/java/com/learn/eureka/ApplicationEurekaProvider2.java
@@ -0,0 +1,21 @@
+package com.learn.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zack.zhang
+ * @projectName learn_cloud_eureka
+ * @title ApplicationEurekaProvider
+ * @package com.learn.eureka
+ * @description eureka服务提供者
+ * @date 2019-06-24 10:26
+ */
+@SpringCloudApplication
+@EnableEurekaClient
+public class ApplicationEurekaProvider2 {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationEurekaProvider2.class, args);
+    }
+}
---
+++ learn_eureka_provider2/src/main/java/com/learn/eureka/controller/HelloController.java
@@ -0,0 +1,38 @@
+package com.learn.eureka.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+/**
+ * @author zack.zhang
+ * @projectName learn_cloud_eureka
+ * @title HelloController
+ * @package com.learn.eureka.controller
+ * @description
+ * @date 2019-07-01 14:30
+ */
+@RestController
+public class HelloController {
+    @Value(value = "${spring.application.name}")
+    private String applicationName;
+
+    @RequestMapping("/hello")
+    public String hello(@RequestBody HashMap param){
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("Hello, this is project name is ");
+        stringBuffer.append(applicationName);
+        stringBuffer.append(",入参是:  ");
+        stringBuffer.append(param.toString());
+        return stringBuffer.toString();
+    }
+    
+    
+    @RequestMapping("/foo")
+    public String foo(String foo) {
+        return "this foo2 is  :" + foo;
+    }
+}
