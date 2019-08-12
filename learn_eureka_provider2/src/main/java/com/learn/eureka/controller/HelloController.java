fca125d5f0701f9ef6f27471e394307df07ab5f4
zack.zhang <zhangwei>
2019-08-12 17:21:00 +0800

    添加方法

--- learn_eureka_provider2/src/main/java/com/learn/eureka/controller/HelloController.java
+++ learn_eureka_provider2/src/main/java/com/learn/eureka/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.learn.eureka.controller;
 
+import com.learn.eureka.entity.Personal;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.HashMap;
 
 /**
@@ -34,5 +37,15 @@ public class HelloController {
     @RequestMapping("/foo")
     public String foo(String foo) {
         return "this foo2 is  :" + foo;
+    }
+    
+    
+    @RequestMapping("/personal")
+    @ResponseBody
+    public Personal getPersonal() {
+        Personal personal = new Personal();
+        personal.setBirth(new Date());
+        personal.setName("张三");
+        return personal;
     }
 }
